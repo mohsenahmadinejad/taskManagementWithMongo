d77eab9dce5535ed2760151585f8302644b9b9c5
mohsenahmadinejad <ahmadi.mohsen.s@gmail.com>
2022-10-13 11:25:03 +0330

    adding Swagger for documentation

--- src/main/java/com/ampada/taskmanagementwithmongo/TaskManagementWithMongoApplication.java
+++ src/main/java/com/ampada/taskmanagementwithmongo/TaskManagementWithMongoApplication.java
@@ -2,8 +2,10 @@ package com.ampada.taskmanagementwithmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TaskManagementWithMongoApplication {
 
     public static void main(String[] args) {
