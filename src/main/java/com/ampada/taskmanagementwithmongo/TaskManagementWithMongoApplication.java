dbf6891e9edfb75562c650ccb1cdba1d18c05f5d
mohsenahmadinejad <ahmadi.mohsen.s@gmail.com>
2022-10-13 11:13:33 +0330

    first commit and init

---
+++ src/main/java/com/ampada/taskmanagementwithmongo/TaskManagementWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.ampada.taskmanagementwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementWithMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementWithMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ampada/taskmanagementwithmongo/TaskManagementWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampada.taskmanagementwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementWithMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
